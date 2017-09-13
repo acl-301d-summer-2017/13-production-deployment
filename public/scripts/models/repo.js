046b60bdc4de9905937bb357da2ab1569b4a10df
Maryussuperboy <martsyalis@gmail.com>
2017-09-13 16:17:47 -0700

    whatevs

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -13,7 +13,7 @@ var app = app || {};
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
     $.ajax({
-      url: '/github/haha'
+      url: '/github/haha',
       type: "GET"
     })
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
